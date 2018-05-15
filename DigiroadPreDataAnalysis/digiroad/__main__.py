94fa4111cb0c43337680121e6ff67db64ebc033a
jeilones <jeisolon.espi@gmail.com>
2018-05-15 11:23:46 +0300

    Start the application as a thread.

--- DigiroadPreDataAnalysis/digiroad/__main__.py
+++ DigiroadPreDataAnalysis/digiroad/__main__.py
@@ -1,3 +1,4 @@
+import threading
 import traceback
 
 import sys
@@ -5,9 +6,21 @@ import sys
 import digiroad.digiroadInit as digiroad
 from digiroad.util import Logger
 
-try:
-    digiroad.main()
-except Exception as err:
-    exc_type, exc_value, exc_traceback = sys.exc_info()
-    lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-    Logger.getInstance().exception(''.join('>> ' + line for line in lines))
+
+class CardatThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print("Starting CAr Routing Data Analysis")
+        try:
+            digiroad.main()
+        except Exception as err:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            Logger.getInstance().exception(''.join('>> ' + line for line in lines))
+            raise err
+
+
+cardatThread = CardatThread()
+cardatThread.start()
